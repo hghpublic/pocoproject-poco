31b6d1e94b6a258cd10743e0d3263f25f818a483
Alex Fabijanic <alex@pocoproject.org>
2026-07-19 18:16:58 +0200

    chore: fix typos in the DLL export boilerplate comments
    this -> This, wheras -> whereas, file see -> file sees in the standard export-macro comment block replicated across library headers. Comment-only change.

--- CppParser/include/Poco/CppParser/CppParser.h
+++ CppParser/include/Poco/CppParser/CppParser.h
@@ -26,8 +26,8 @@
 //
 // The following block is the standard way of creating macros which make exporting
 // from a DLL simpler. All files within this DLL are compiled with the CppParser_EXPORTS
-// symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file sees
 // CppParser_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
 //
